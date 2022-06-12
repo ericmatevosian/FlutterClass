ea528a5824332f3fcba0b1672c40ae4e0e2d9c87
ericmatevosian <matevosian.eric@gmail.com>
2022-06-12 10:48:24 +0430

    List Variable Created

---
+++ 14010321-ListVariable/bin/dartfirst.dart
@@ -0,0 +1,17 @@
+void main() {
+var friendsname=['Narek' , 'Karin', 'Orbel', 'Reymond'];
+var friendsage=[30,33,31,29];
+
+print(friendsname);
+print(friendsname[0]);
+print(friendsage[0]);
+
+print(friendsname[1]);
+print(friendsage[1]);
+
+print(friendsname[2]);
+print(friendsage[2]);
+
+print(friendsname[3]);
+print(friendsage[3]);
+}
